2fc350ee8dec2dad1312667ad77114cae3f38d64
Bryan Campbell <bryanjcampbell1@gmail.com>
2017-01-22 22:12:34 -0500

    collection view appears.  problems however with label...but ultimately i dont need a label

---
+++ GuidePost/NewCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewCollectionViewCell.swift
+//  GuidePost
+//
+//  Created by Bryan Campbell on 1/22/17.
+//  Copyright © 2017 Bryan Campbell. All rights reserved.
+//
+
+import UIKit
+
+class NewCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+
+}
